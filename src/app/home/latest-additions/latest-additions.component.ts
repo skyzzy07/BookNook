a23228417fc956c62614161e290bf8ff655fc7fb
skyzzy07 <youssridali07@gmail.com>
2024-06-23 23:22:55 +0100

    update home page

---
+++ src/app/home/latest-additions/latest-additions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-additions',
+  templateUrl: './latest-additions.component.html',
+  styleUrls: ['./latest-additions.component.css']
+})
+export class LatestAdditionsComponent {
+
+}
